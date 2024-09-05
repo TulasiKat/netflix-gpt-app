056145438bfbfd971f0f478df39b4ff993906f0a
Tulasi Katukoju <tulasikatukoju@Tulasis-MacBook-Pro.local>
2024-09-05 21:57:04 +0530

    correction in prev commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyByCoiaaV7FezEsJXlVri5NkE892BC7MAw",
+  apiKey: import.meta.env.VITE_FIRE_BASE_API_KEY,
   authDomain: "netflixgpt-39666.firebaseapp.com",
   projectId: "netflixgpt-39666",
   storageBucket: "netflixgpt-39666.appspot.com",
